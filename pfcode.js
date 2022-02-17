1673aa88e9c94ba06ac6ec57ed8bb570aaa95f21
swng <stevenw1@andrew.cmu.edu>
2022-02-16 19:11:06 -0600

    formatting

--- pfcode.js
+++ pfcode.js
@@ -1,20 +1,27 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+var _____WB$wombat$assign$function_____ = function (name) {
+	return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name];
+};
+if (!self.__WB_pmw) {
+	self.__WB_pmw = function (obj) {
+		this.__WB_source = obj;
+		return this;
+	};
+}
 {
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
+	let window = _____WB$wombat$assign$function_____('window');
+	let self = _____WB$wombat$assign$function_____('self');
+	let document = _____WB$wombat$assign$function_____('document');
+	let location = _____WB$wombat$assign$function_____('location');
+	let top = _____WB$wombat$assign$function_____('top');
+	let parent = _____WB$wombat$assign$function_____('parent');
+	let frames = _____WB$wombat$assign$function_____('frames');
+	let opener = _____WB$wombat$assign$function_____('opener');
 
-// Add-on
-// Conversion into diagram code
-// Rev.3
+	// Add-on
+	// Conversion into diagram code
+	// Rev.3
 
-/*
+	/*
 Jan 13, 2011 18:11
 Help with fumen
 
@@ -26,50 +33,51 @@ Please contact (PM) message me at harddrop.com/chopin if you can.
 Big help appreciated!
 */
 
-if(typeof STRING_WIKICODE_OUTPUT    == 'undefined') STRING_WIKICODE_OUTPUT    = "Wikiコード出力";
-if(typeof STRING_WIKICODE_UPPERCASE == 'undefined') STRING_WIKICODE_UPPERCASE = "大文字";
+	if (typeof STRING_WIKICODE_OUTPUT == 'undefined') STRING_WIKICODE_OUTPUT = 'Wikiコード出力';
+	if (typeof STRING_WIKICODE_UPPERCASE == 'undefined') STRING_WIKICODE_UPPERCASE = '大文字';
 
-addon_ui += '<hr width=90% size=1>';
-addon_ui += '<table border=0 cellspacing=0 cellpadding=0>';
-addon_ui += '<td><input type=button value="'+STRING_WIKICODE_OUTPUT+'" onclick="outputpfcode();">';
-addon_ui += '<td><input type=checkbox name=pfucase id=pfucase checked><label for=pfucase>'+STRING_WIKICODE_UPPERCASE+'</label><td>';
-addon_ui += '</table>';
-addon_ui += '<textarea name=pfcode id=pfcode cols=32 rows=4 style="font-size:9pt;" onfocus="this.select();"></textarea><br>';
+	addon_ui += '<hr width=90% size=1>';
+	addon_ui += '<table border=0 cellspacing=0 cellpadding=0>';
+	addon_ui += '<td><input type=button value="' + STRING_WIKICODE_OUTPUT + '" onclick="outputpfcode();">';
+	addon_ui +=
+		'<td><input type=checkbox name=pfucase id=pfucase checked><label for=pfucase>' +
+		STRING_WIKICODE_UPPERCASE +
+		'</label><td>';
+	addon_ui += '</table>';
+	addon_ui +=
+		'<textarea name=pfcode id=pfcode cols=32 rows=4 style="font-size:9pt;" onfocus="this.select();"></textarea><br>';
 
-function outputpfcode()
-{
-	var pfcode = document.getElementById("pfcode");
-	var pfucase = document.getElementById("pfucase");
-	var pfout = "";
+	function outputpfcode() {
+		var pfcode = document.getElementById('pfcode');
+		var pfucase = document.getElementById('pfucase');
+		var pfout = '';
 
-	var tf = new Array(fldblks);
-	for (var i = 0; i < fldblks; i++) tf[i] = f[i] + ((pfucase.checked && f[i] >= 1 && f[i] <= 8) ? 8 : 0);
-	if (p[0] > 0) {
-		for (var i = 0; i < 4; i++) {
-			tf[p[2] + b[p[0] * 32 + p[1] * 8 + i * 2 + 1] * 10 + b[p[0] * 32 + p[1] * 8 + i * 2] - 11] = p[0] + 8;
+		var tf = new Array(fldblks);
+		for (var i = 0; i < fldblks; i++) tf[i] = f[i] + (pfucase.checked && f[i] >= 1 && f[i] <= 8 ? 8 : 0);
+		if (p[0] > 0) {
+			for (var i = 0; i < 4; i++) {
+				tf[p[2] + b[p[0] * 32 + p[1] * 8 + i * 2 + 1] * 10 + b[p[0] * 32 + p[1] * 8 + i * 2] - 11] = p[0] + 8;
+			}
 		}
-	}
 
-	var st = fldlines - 2;
-	for (var j = fldlines - 2; j >= 0; j--) {
-		for (var i = 0; i < 10; i++) {
-			if (tf[j * 10 + i]) st = j - (j > 0);
+		var st = fldlines - 2;
+		for (var j = fldlines - 2; j >= 0; j--) {
+			for (var i = 0; i < 10; i++) {
+				if (tf[j * 10 + i]) st = j - (j > 0);
+			}
 		}
-	}
 
-	pfout += "|{{pfstart}}\n";
-	for (var j = st; j < fldlines - 1; j++) {
-		pfout += "{{pfrow";
-		for (var i = 0; i < 10; i++) {
-			pfout += "|" + " iloztjsgILOZTJSG".charAt(tf[j * 10 + i]);
+		pfout += '|{{pfstart}}\n';
+		for (var j = st; j < fldlines - 1; j++) {
+			pfout += '{{pfrow';
+			for (var i = 0; i < 10; i++) {
+				pfout += '|' + ' iloztjsgILOZTJSG'.charAt(tf[j * 10 + i]);
+			}
+			pfout += '}}\n';
 		}
-		pfout += "}}\n";
+		pfout += '{{pfend}}\n';
+
+		pfcode.value = pfout;
+		pfcode.focus();
 	}
-	pfout += "{{pfend}}\n";
-
-	pfcode.value = pfout;
-	pfcode.focus();
-}
-
-
 }
