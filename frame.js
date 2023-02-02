bc95f5a28fd03569415741dc0d9e70318d1394d8
swng <stevenw1@andrew.cmu.edu>
2023-02-02 10:30:44 -0600

    v115e with the relevant addons by default

--- frame.js
+++ frame.js
@@ -1,106 +1,81 @@
-var _____WB$wombat$assign$function_____ = function (name) {
-	return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name];
-};
-if (!self.__WB_pmw) {
-	self.__WB_pmw = function (obj) {
-		this.__WB_source = obj;
-		return this;
-	};
+// Add-on
+// Extended frame management
+// Rev.2
+
+if(typeof STRING_FRAME_INSERT    == 'undefined') STRING_FRAME_INSERT    = "＋ 現ページ複製";
+if(typeof STRING_FRAME_DELETE    == 'undefined') STRING_FRAME_DELETE    = "－ 現ページ削除";
+if(typeof STRING_FRAME_DELPAST   == 'undefined') STRING_FRAME_DELPAST   = "／ 前ページまでを削除";
+if(typeof STRING_FRAME_IMPORT    == 'undefined') STRING_FRAME_IMPORT    = "＃ 追加読み込み";
+if(typeof STRING_DELPAST_CONFIRM == 'undefined') STRING_DELPAST_CONFIRM = "前ページまでを完全に削除してよろしいですか？";
+
+addon_ui += '<hr width=90% size=1>';
+addon_ui += '<input type=button value="'+STRING_FRAME_DELETE+'" onclick="deletepage();dblclickchk=1;" ondblclick="if(dblclickchk==0)deletepage();"><br>';
+addon_ui += '<input type=button value="'+STRING_FRAME_INSERT+'" onclick="insertpage();dblclickchk=1;" ondblclick="if(dblclickchk==0)insertpage();"><br>';
+addon_ui += '<input type=button value="'+STRING_FRAME_DELPAST+'" onclick="delpastpage();"><br>';
+addon_ui += '<input type=button value="'+STRING_FRAME_IMPORT+'" onclick="importpage();"><br>';
+
+function copyframe(srcframe,dstframe){
+  for(var pfi=0;pfi<fldblks;pfi++)af[dstframe*fldblks+pfi]=af[srcframe*fldblks+pfi];
+  for(var pfi=0;pfi<3;pfi++)ap[dstframe*3+pfi]=ap[srcframe*3+pfi];
+  au[dstframe]=au[srcframe];
+  am[dstframe]=am[srcframe];
+  ac[dstframe]=ac[srcframe];
+  ad[dstframe]=ad[srcframe];
 }
+
+function insertpage()
 {
-	let window = _____WB$wombat$assign$function_____('window');
-	let self = _____WB$wombat$assign$function_____('self');
-	let document = _____WB$wombat$assign$function_____('document');
-	let location = _____WB$wombat$assign$function_____('location');
-	let top = _____WB$wombat$assign$function_____('top');
-	let parent = _____WB$wombat$assign$function_____('parent');
-	let frames = _____WB$wombat$assign$function_____('frames');
-	let opener = _____WB$wombat$assign$function_____('opener');
+  var dc=document.getElementById("dc");
+  if(framemax>=framelim-1)return;
+  pushframe(frame);
+  for(var i=framemax;i>=frame;i--){
+    copyframe(i,i+1);
+  }
+  framemax++;
+  ad[frame]=true;
+  frame++;
+  popframe(frame);
+  updated();
+  refreshPage();
+  refresh();
+}
 
-	// Add-on
-	// Extended frame management
-	// Rev.2
+function deletepage()
+{
+  if(frame>=framemax)return;
+  for(var i=frame;i<framemax;i++){
+    copyframe(i+1,i);
+  }
+  framemax--;
+  popframe(frame);
+  updated();
+  refreshPage();
+  refresh();
+}
 
-	if (typeof STRING_FRAME_INSERT == 'undefined') STRING_FRAME_INSERT = '＋ 現ページ複製';
-	if (typeof STRING_FRAME_DELETE == 'undefined') STRING_FRAME_DELETE = '－ 現ページ削除';
-	if (typeof STRING_FRAME_DELPAST == 'undefined') STRING_FRAME_DELPAST = '／ 前ページまでを削除';
-	if (typeof STRING_FRAME_IMPORT == 'undefined') STRING_FRAME_IMPORT = '＃ 追加読み込み';
-	if (typeof STRING_DELPAST_CONFIRM == 'undefined')
-		STRING_DELPAST_CONFIRM = '前ページまでを完全に削除してよろしいですか？';
+function delpastpage(){
+  if(frame<=0)return;
+  if(confirm(STRING_DELPAST_CONFIRM)!=1)return;
+  pushframe(frame);
+  for(var i=0;i<=framemax-frame;i++){
+    copyframe(i+frame,i);
+  }
+  framemax=framemax-frame;
+  frame=0;
+  updated();
+  refreshPage();
+}
 
-	addon_ui += '<hr width=90% size=1>';
-	addon_ui +=
-		'<input type=button value="' +
-		STRING_FRAME_DELETE +
-		'" onclick="deletepage();dblclickchk=1;" ondblclick="if(dblclickchk==0)deletepage();"><br>';
-	addon_ui +=
-		'<input type=button value="' +
-		STRING_FRAME_INSERT +
-		'" onclick="insertpage();dblclickchk=1;" ondblclick="if(dblclickchk==0)insertpage();"><br>';
-	addon_ui += '<input type=button value="' + STRING_FRAME_DELPAST + '" onclick="delpastpage();"><br>';
-	addon_ui += '<input type=button value="' + STRING_FRAME_IMPORT + '" onclick="importpage();"><br>';
-
-	function copyframe(srcframe, dstframe) {
-		for (var pfi = 0; pfi < fldblks; pfi++) af[dstframe * fldblks + pfi] = af[srcframe * fldblks + pfi];
-		for (var pfi = 0; pfi < 3; pfi++) ap[dstframe * 3 + pfi] = ap[srcframe * 3 + pfi];
-		au[dstframe] = au[srcframe];
-		am[dstframe] = am[srcframe];
-		ac[dstframe] = ac[srcframe];
-		ad[dstframe] = ad[srcframe];
-	}
-
-	function insertpage() {
-		var dc = document.getElementById('dc');
-		if (framemax >= framelim - 1) return;
-		pushframe(frame);
-		for (var i = framemax; i >= frame; i--) {
-			copyframe(i, i + 1);
-		}
-		framemax++;
-		ad[frame] = true;
-		frame++;
-		popframe(frame);
-		updated();
-		refreshPage();
-		refresh();
-	}
-
-	function deletepage() {
-		if (frame >= framemax) return;
-		for (var i = frame; i < framemax; i++) {
-			copyframe(i + 1, i);
-		}
-		framemax--;
-		popframe(frame);
-		updated();
-		refreshPage();
-		refresh();
-	}
-
-	function delpastpage() {
-		if (frame <= 0) return;
-		if (confirm(STRING_DELPAST_CONFIRM) != 1) return;
-		pushframe(frame);
-		for (var i = 0; i <= framemax - frame; i++) {
-			copyframe(i + frame, i);
-		}
-		framemax = framemax - frame;
-		frame = 0;
-		updated();
-		refreshPage();
-	}
-
-	function importpage() {
-		if (framemax < framelim - 1) {
-			importframemax = framemax;
-			importupdateflag = updateflag;
-			pgnext(2);
-			pgnext(0);
-			updateflag = 0;
-			versioncheck(frame);
-			if (framemax > framelim - 1) framemax = framelim - 1;
-			if (importupdateflag || framemax != importframemax) updated();
-			refreshPage();
-		}
-	}
+function importpage(){
+  if(framemax<framelim-1){
+    importframemax=framemax;
+    importupdateflag=updateflag;
+    pgnext(2);
+    pgnext(0);
+    updateflag=0;
+    versioncheck(frame);
+    if(framemax>framelim-1)framemax=framelim-1;
+    if(importupdateflag||framemax!=importframemax)updated();
+    refreshPage();
+  }
 }
--- pfcode.js
+++ pfcode.js
@@ -1,27 +1,8 @@
-var _____WB$wombat$assign$function_____ = function (name) {
-	return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name];
-};
-if (!self.__WB_pmw) {
-	self.__WB_pmw = function (obj) {
-		this.__WB_source = obj;
-		return this;
-	};
-}
-{
-	let window = _____WB$wombat$assign$function_____('window');
-	let self = _____WB$wombat$assign$function_____('self');
-	let document = _____WB$wombat$assign$function_____('document');
-	let location = _____WB$wombat$assign$function_____('location');
-	let top = _____WB$wombat$assign$function_____('top');
-	let parent = _____WB$wombat$assign$function_____('parent');
-	let frames = _____WB$wombat$assign$function_____('frames');
-	let opener = _____WB$wombat$assign$function_____('opener');
+// Add-on
+// Conversion into diagram code
+// Rev.4
 
-	// Add-on
-	// Conversion into diagram code
-	// Rev.3
-
-	/*
+/*
 Jan 13, 2011 18:11
 Help with fumen
 
@@ -33,51 +14,79 @@ Please contact (PM) message me at harddrop.com/chopin if you can.
 Big help appreciated!
 */
 
-	if (typeof STRING_WIKICODE_OUTPUT == 'undefined') STRING_WIKICODE_OUTPUT = 'Wikiコード出力';
-	if (typeof STRING_WIKICODE_UPPERCASE == 'undefined') STRING_WIKICODE_UPPERCASE = '大文字';
+if(typeof STRING_WIKICODE_OUTPUT    == 'undefined') STRING_WIKICODE_OUTPUT    = "Wikiコード出力";
+if(typeof STRING_WIKICODE_UPPERCASE == 'undefined') STRING_WIKICODE_UPPERCASE = "大文字";
+if(typeof STRING_WIKICODE_ALLPAGES  == 'undefined') STRING_WIKICODE_ALLPAGES  = "全ページ";
+if(typeof STRING_WIKICODE_ALIGN     == 'undefined') STRING_WIKICODE_ALIGN     = "高さを揃える";
+if(typeof STRING_WIKICODE_COMMENTS  == 'undefined') STRING_WIKICODE_COMMENTS  = "コメント";
 
-	addon_ui += '<hr width=90% size=1>';
-	addon_ui += '<table border=0 cellspacing=0 cellpadding=0>';
-	addon_ui += '<td><input type=button value="' + STRING_WIKICODE_OUTPUT + '" onclick="outputpfcode();">';
-	addon_ui +=
-		'<td><input type=checkbox name=pfucase id=pfucase checked><label for=pfucase>' +
-		STRING_WIKICODE_UPPERCASE +
-		'</label><td>';
-	addon_ui += '</table>';
-	addon_ui +=
-		'<textarea name=pfcode id=pfcode cols=32 rows=4 style="font-size:9pt;" onfocus="this.select();"></textarea><br>';
+addon_ui += '<hr width=90% size=1>';
+addon_ui += '<table border=0 cellspacing=0 cellpadding=0>';
+addon_ui += '<tr>';
+addon_ui += '<td>';
+addon_ui += '<input type=checkbox name=pfucase id=pfucase checked><label for=pfucase>'+STRING_WIKICODE_UPPERCASE+'</label>';
+addon_ui += '<input type=checkbox name=pfallpg id=pfallpg><label for=pfallpg>'+STRING_WIKICODE_ALLPAGES+'</label>';
+addon_ui += '</td>';
+addon_ui += '</tr>';
+addon_ui += '<tr>';
+addon_ui += '<td>';
+addon_ui += '<input type=checkbox name=pfalign id=pfalign checked><label for=pfalign>'+STRING_WIKICODE_ALIGN+'</label>';
+addon_ui += '<input type=checkbox name=pfcm id=pfcm><label for=pfcm>'+STRING_WIKICODE_COMMENTS+'</label>';
+addon_ui += '</td>';
+addon_ui += '</tr>';
+addon_ui += '<tr>';
+addon_ui += '<td>';
+addon_ui += '<input type=button value="'+STRING_WIKICODE_OUTPUT+'" onclick="outputpfcode();">';
+addon_ui += '</td>';
+addon_ui += '</tr>';
+addon_ui += '</table>';
+addon_ui += '<textarea name=pfcode id=pfcode cols=32 rows=4 style="font-size:9pt;" onfocus="this.select();"></textarea><br>';
 
-	function outputpfcode() {
-		var pfcode = document.getElementById('pfcode');
-		var pfucase = document.getElementById('pfucase');
-		var pfout = '';
+function outputpfcode()
+{
+	var pfcode = document.getElementById("pfcode");
+	var pfucase = document.getElementById("pfucase");
+	var pfallpg = document.getElementById("pfallpg");
+	var pfalign = document.getElementById("pfalign");
+	var pfcm = document.getElementById("pfcm");
+	var pfout = "";
 
-		var tf = new Array(fldblks);
-		for (var i = 0; i < fldblks; i++) tf[i] = f[i] + (pfucase.checked && f[i] >= 1 && f[i] <= 8 ? 8 : 0);
-		if (p[0] > 0) {
-			for (var i = 0; i < 4; i++) {
-				tf[p[2] + b[p[0] * 32 + p[1] * 8 + i * 2 + 1] * 10 + b[p[0] * 32 + p[1] * 8 + i * 2] - 11] = p[0] + 8;
+    pushframe(frame);
+	var st = fldlines - 2;
+	for (var s = 0; s < 2; s++) {
+		for (var k = 0; k <= framemax; k++) {
+			var tf = new Array(fldblks);
+			for (var i = 0; i < fldblks; i++) tf[i] = af[k * fldblks + i] + ((pfucase.checked && af[k * fldblks + i] >= 1 && af[k * fldblks + i] <= 8) ? 8 : 0);
+			if (ap[k * 3 + 0] > 0) {
+				for (var i = 0; i < 4; i++) {
+					tf[ap[k * 3 + 2] + b[ap[k * 3 + 0] * 32 + ap[k * 3 + 1] * 8 + i * 2 + 1] * 10 + b[ap[k * 3 + 0] * 32 + ap[k * 3 + 1] * 8 + i * 2] - 11] = ap[k * 3 + 0] + 8;
+				}
+			}
+		
+			if (!pfalign.checked) st = fldlines - 2;
+			if (s == 0 || !pfalign.checked) {
+				for (var j = fldlines - 2; j >= 0; j--) {
+					for (var i = 0; i < 10; i++) {
+						if (tf[j * 10 + i]) if (st > j - (j > 0)) st = j - (j > 0);
+					}
+				}
+			}
+		
+			if (s == 1 && (pfallpg.checked || k == frame)) {
+				pfout += "|{{pfstart}}\n";
+				for (var j = st; j < fldlines - 1; j++) {
+					pfout += "{{pfrow";
+					for (var i = 0; i < 10; i++) {
+						pfout += "|" + " iloztjsgILOZTJSG".charAt(tf[j * 10 + i]);
+					}
+					pfout += "}}\n";
+				}
+				pfout += "{{pfend}}\n";
+				if (pfcm.checked && ac[k] != "") pfout += ac[k] + "\n";
 			}
 		}
-
-		var st = fldlines - 2;
-		for (var j = fldlines - 2; j >= 0; j--) {
-			for (var i = 0; i < 10; i++) {
-				if (tf[j * 10 + i]) st = j - (j > 0);
-			}
-		}
-
-		pfout += '|{{pfstart}}\n';
-		for (var j = st; j < fldlines - 1; j++) {
-			pfout += '{{pfrow';
-			for (var i = 0; i < 10; i++) {
-				pfout += '|' + ' iloztjsgILOZTJSG'.charAt(tf[j * 10 + i]);
-			}
-			pfout += '}}\n';
-		}
-		pfout += '{{pfend}}\n';
-
-		pfcode.value = pfout;
-		pfcode.focus();
 	}
+
+	pfcode.value = pfout;
+	pfcode.focus();
 }
